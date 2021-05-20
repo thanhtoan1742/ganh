d36fa5769979ede6f17d5ff61b0751d57cf4e375
thanhtoan1742 <thanhtoan1742@gmail.com>
2021-05-20 15:07:57 +0700

    refactored

--- src/board.py
+++ src/board.py
@@ -1,9 +1,6 @@
 import numpy as np
 
 class board:
-    #   7 0 1
-    #   6   2
-    #   5 4 3
     dx = [-1, -1, 0, 1, 1, 1, 0, -1]
     dy = [0, 1, 1, 1, 0, -1, -1, -1]
 
@@ -40,7 +37,6 @@ class board:
             '   | / | \ | / | \ |',
             '4  ' + lines[4]
         ])
-
         print(s, end='\n\n')
 
     def get_current_player(self):
@@ -64,9 +60,16 @@ class board:
     def _in_range_(self, x, y):
         return x >= 0 and y >= 0 and x < 5 and y < 5
 
-    #   3 0 1
-    #   2   2
-    #   1 0 3
+    def _neighbor_(self, x, y):
+        for i in range(8):
+            if (x + y)%2 == 1 and i%2 == 1:
+                continue
+
+            u = x + board.dx[i]
+            v = y + board.dy[i]
+            if self._in_range_(u, v):
+                yield u, v
+
     def _neighbor_pair_(self, x, y):
         for i in range(4):
             ax = x + board.dx[i]
@@ -89,51 +92,6 @@ class board:
                 continue
             yield ax, ay, bx, by
 
-
-    def _neighbor_(self, x, y):
-        for i in range(8):
-            if (x + y)%2 == 1 and i%2 == 1:
-                continue
-
-            u = x + board.dx[i]
-            v = y + board.dy[i]
-            if self._in_range_(u, v):
-                yield u, v
-
-
-    def _check_carry_(self, x, y):
-        for ax, ay, bx, by in self._carry_neighbor_pair_(x, y):
-            self.board[ax][ay] = self.board[x][y]
-            self.board[bx][by] = self.board[x][y]
-
-    def DFS(self, x, y):
-        self.connected_component[x][y] = self.n_connected_component
-        for u, v in self._neighbor_(x, y):
-            if self.connected_component[u][v] != 0:
-                continue
-            if self.board[u][v] == 0:
-                self.n_reachable_empty += 1
-            if self.board[u][v] == self.board[x][y]:
-                self.DFS(u, v)
-
-    def _check_surround_(self):
-        self.connected_component = [[0 for y in range(5)] for x in range(5)]
-        self.n_connected_component = 0
-        reachable_empty = [-1]
-        for x in range(5):
-            for y in range(5):
-                if self.connected_component[x][y] != 0 or self.board[x][y] == 0:
-                    continue
-                self.n_reachable_empty = 0
-                self.n_connected_component += 1
-                self.DFS(x, y)
-                reachable_empty.append(self.n_reachable_empty)
-
-        for x in range(5):
-            for y in range(5):
-                if reachable_empty[self.connected_component[x][y]] == 0:
-                    self.board[x][y] = 0 - self.board[x][y]
-
     def _open_moves_(self):
         for x in range(5):
             for y in range(5):
@@ -147,6 +105,38 @@ class board:
                     if len(list(self._carry_neighbor_pair_(u, v))) > 0:
                         yield (x, y), (u, v)
 
+    def _check_carry_(self, x, y):
+        for ax, ay, bx, by in self._carry_neighbor_pair_(x, y):
+            self.board[ax][ay] = self.board[x][y]
+            self.board[bx][by] = self.board[x][y]
+
+    def _DFS_(self, x, y):
+        self.connected_component[x][y] = self.n_connected_component
+        for u, v in self._neighbor_(x, y):
+            if self.connected_component[u][v] != 0:
+                continue
+            if self.board[u][v] == 0:
+                self.n_reachable_empty += 1
+            if self.board[u][v] == self.board[x][y]:
+                self._DFS_(u, v)
+
+    def _check_surround_(self):
+        self.connected_component = [[0 for y in range(5)] for x in range(5)]
+        self.n_connected_component = 0
+        reachable_empty = [-1]
+        for x in range(5):
+            for y in range(5):
+                if self.connected_component[x][y] != 0 or self.board[x][y] == 0:
+                    continue
+                self.n_reachable_empty = 0
+                self.n_connected_component += 1
+                self._DFS_(x, y)
+                reachable_empty.append(self.n_reachable_empty)
+
+        for x in range(5):
+            for y in range(5):
+                if reachable_empty[self.connected_component[x][y]] == 0:
+                    self.board[x][y] = 0 - self.board[x][y]
 
     def make_move(self, move):
         (sx, sy), (tx, ty) = move
@@ -161,7 +151,6 @@ class board:
         open_moves = list(self._open_moves_())
         if len(open_moves) > 0 and not (move in open_moves):
             raise KeyError('player has to play open move')
-
 
         self.board[sx][sy] = 0
         self.board[tx][ty] = self.current_player
--- src/random_player.py
+++ src/random_player.py
@@ -1,4 +1,5 @@
 from random import choice
+
 dx = [-1, -1, 0, 1, 1, 1, 0, -1]
 dy = [0, 1, 1, 1, 0, -1, -1, -1]
 
