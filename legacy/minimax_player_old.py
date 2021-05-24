864eaa2f4294c75b46c3ea08a3958a3c1b7759d2
hdthinh1012 <hdthinh01@gmail.com>
2021-05-24 19:34:24 +0700

    "Update minimax"

--- legacy/minimax_player_old.py
+++ legacy/minimax_player_old.py
@@ -1,10 +1,11 @@
 import numpy as np, copy
+from board import *
 from random import choice
 
 INF = 10000
 START_MAX_DEPTH = 3
-MID_MAX_DEPTH = 3
-END_MAX_DEPTH = 3
+MID_MAX_DEPTH = 2
+END_MAX_DEPTH = 2
 
 dx = [-1, -1, 0, 1, 1, 1, 0, -1]
 dy = [0, 1, 1, 1, 0, -1, -1, -1]
@@ -187,161 +188,24 @@ def get_all_possible_move(board, player, last_move):
     # print(all_possible_moves)
     return all_possible_moves
 
-'''
-    DEFINE MOVE STATE FUNCTION
-'''
-
-def _in_range_(x, y):
-    return x >= 0 and y >= 0 and x < 5 and y < 5
-
-
-def _neighbor_(x, y):
-    for i in range(8):
-        if (x + y)%2 == 1 and i%2 == 1:
-            continue
-
-        u = x + dx[i]
-        v = y + dy[i]
-        if _in_range_(u, v):
-            yield u, v
-
-
-def _neighbor_pair_(x, y):
-    res = []
-    for i in range(4):
-        ax = x + dx[i]
-        ay = y + dy[i]
-        if not _in_range_(ax, ay):
-            continue
-
-        bx = x + dx[(i + 4)%8]
-        by = y + dy[(i + 4)%8]
-        if not _in_range_(bx, by):
-            continue
-
-        res.append((ax, ay, bx, by))
-    return res
-
-
-def _carry_neighbor_pair_(board, x, y, player):
-    res = []
-    for ax, ay, bx, by in _neighbor_pair_(x, y):
-        if board[ax][ay] != board[bx][by]:
-            continue
-        if board[ax][ay] != 0 - player:
-            continue
-        res.append((ax, ay, bx, by))
-    return res
-
-
-def _check_open_move_(board, sx, sy, tx, ty, last_move, player):
-
-    u, v, _, _ = last_move
-    if len(_carry_neighbor_pair_(board, u, v, player)) == 0:
-        return True
-
-    possible_starting_position = [
-        (a, b)
-        for a, b in _neighbor_(u, v)
-        if board[a][b] == player
-    ]
-    if len(possible_starting_position) == 0:
-        return True
-
-    return ((sx, sy) in possible_starting_position) and (u == tx) and (v == ty)
-
-
-def _check_carry_(board, x, y, player):
-    for ax, ay, bx, by in _carry_neighbor_pair_(board, x, y, player):
-        board[ax][ay] = board[x][y]
-        board[bx][by] = board[x][y]
-        return board
-
-
-def _DFS_(board, x, y):
-        global connected_component, n_connected_component, reachable_empty, n_reachable_empty
-        connected_component[x][y] = n_connected_component
-        for u, v in _neighbor_(x, y):
-            if connected_component[u][v] != 0:
-                continue
-            if board[u][v] == 0:
-                n_reachable_empty += 1
-            if board[u][v] == board[x][y]:
-                _DFS_(board, u, v)
-
-
-connected_component = None
-n_connected_component = 0
-reachable_empty = [-1]
-n_reachable_empty = 0
-
-def _check_surround_(board):
-    global connected_component, n_connected_component, reachable_empty, n_reachable_empty
-
-    connected_component = [[0 for y in range(5)] for x in range(5)]
-    n_connected_component = 0
-    reachable_empty = [-1]
-    for x in range(5):
-        for y in range(5):
-            if connected_component[x][y] != 0 or board[x][y] == 0:
-                continue
-            n_reachable_empty = 0
-            n_connected_component += 1
-            _DFS_(board, x, y)
-            reachable_empty.append(n_reachable_empty)
-
-    for x in range(5):
-        for y in range(5):
-            if reachable_empty[connected_component[x][y]] == 0:
-                board[x][y] = 0 - board[x][y]
-
-    return board
-
-
-def make_move(board, move, player, last_move):
-    sx, sy, tx, ty = move
-    # print(f'move from ({sx}, {sy}) to ({tx}, {ty})')
-
-    new_board = copy.deepcopy(board)
-
-    if new_board[sx][sy] != player:
-        raise Exception('player in starting position does not match current player')
-    if new_board[tx][ty] != 0:
-        raise Exception('destination is not empty')
-    if not (tx, ty) in _neighbor_(sx, sy):
-        raise Exception('destination is not reachable from starting position')
-
-    if last_move and not _check_open_move_(board, sx, sy, tx, ty, last_move, player):
-        raise Exception('player has to play open move')
-
-    new_board[sx][sy] = 0
-    new_board[tx][ty] = player
-
-    _check_carry_(new_board, tx, ty, player)
-    _check_surround_(new_board)
-    return new_board
-
-
-'''
-    DEFINE NEW MINIMAX
-'''
 
 def minimax(board, move_list, player, tree_depth):
     """
     Returns the optimal move for the current player on the board.
     """
-    if abs(evaluate(board)) == 16: 
+    if board.finished(): 
         return None
     optimal_move = None
     alpha = -INF  # current_worst_max_possible
     beta = INF   # current_worst_min_possible
-    last_move = moves[-1] if len(moves) > 0 else None
 
     if player == 1:
         v = -INF
         for move in move_list:
-            new_board = make_move(board, move, player, last_move)
-            new_v = min_value(new_board, alpha, beta, 0 - player, tree_depth, move)
+            new_board = copy.deepcopy(board)
+            x1, y1, x2, y2 = move
+            new_board.make_move(((x1,y1),( x2,y2)))
+            new_v = min_value(new_board, alpha, beta, 0 - player, tree_depth)
             if new_v > v:
                 v = new_v
                 alpha = v
@@ -349,8 +213,10 @@ def minimax(board, move_list, player, tree_depth):
     else:
         v = INF
         for move in move_list:
-            new_board = make_move(board, move, player, last_move)
-            new_v = max_value(new_board, alpha, beta, 0 - player, tree_depth, move)
+            new_board = copy.deepcopy(board)
+            x1, y1, x2, y2 = move
+            new_board.make_move(((x1,y1),( x2,y2)))
+            new_v = max_value(new_board, alpha, beta, 0 - player, tree_depth)
             if new_v < v:
                 v = new_v
                 beta = v
@@ -360,14 +226,20 @@ def minimax(board, move_list, player, tree_depth):
     return optimal_move
 
 
-def max_value(board, alpha, beta, player, depth, last_move):
-    if abs(evaluate(board)) == 16 or depth == 0:
-        return evaluate(board) * (1 + depth / 10)
+def max_value(board, alpha, beta, player, depth):
+    if board.finished() or depth == 0:
+        return evaluate(board.board)
     v = -INF
-    move_list = get_all_possible_move(board, player, last_move)
+    start, end = board.moves[-1]
+    x1, y1 = start
+    x2, y2 = end
+    last_move = (x1,y1,x2,y2)
+    move_list = get_all_possible_move(board.board, player, last_move)
     for move in move_list:
-        new_board = make_move(board, move, player, last_move)
-        new_v = min_value(new_board, alpha, beta, 0 - player, depth - 1, move)
+        new_board = copy.deepcopy(board)
+        x1, y1, x2, y2 = move
+        new_board.make_move(((x1,y1),( x2,y2)))
+        new_v = min_value(new_board, alpha, beta, 0 - player, depth - 1)
         v = max(v, new_v) 
         alpha = max(new_v, alpha)
         if alpha > beta:
@@ -376,14 +248,20 @@ def max_value(board, alpha, beta, player, depth, last_move):
     return v
 
 
-def min_value(board, alpha, beta, player, depth, last_move):
-    if abs(evaluate(board)) == 16 or depth == 0:
-        return evaluate(board) * (1 + depth / 10)
+def min_value(board, alpha, beta, player, depth):
+    if board.finished() or depth == 0:
+        return evaluate(board.board)
     v = INF
-    move_list = get_all_possible_move(board, player, last_move)
+    start, end = board.moves[-1]
+    x1, y1 = start
+    x2, y2 = end
+    last_move = (x1,y1,x2,y2)
+    move_list = get_all_possible_move(board.board, player, last_move)
     for move in move_list:
-        new_board = make_move(board, move, player, last_move)
-        new_v = max_value(new_board, alpha, beta, 0 - player, depth - 1, move)
+        new_board = copy.deepcopy(board)
+        x1, y1, x2, y2 = move
+        new_board.make_move(((x1,y1),( x2,y2)))
+        new_v = max_value(new_board, alpha, beta, 0 - player, depth - 1)
         v = min(v, new_v)
         beta = min(new_v, beta)
         if beta < alpha:
@@ -411,16 +289,20 @@ def move(board_ndarray, player):
         last_move = None
 
     all_moves = get_all_possible_move(board_ndarray, player, last_move)
+    board_class = board()
+    board_class.board = board_ndarray
+    board_class.current_player = player
 
-    if evaluate(board_ndarray) * player < 12: 
+    if evaluate(board_class.board) * player < 12: 
         # Opponent has more than 2 units left
-        optimal_move = minimax(board_ndarray, all_moves, player, START_MAX_DEPTH)
-    elif evaluate(board_ndarray) * player < 14: 
+        optimal_move = minimax(board_class, all_moves, player, START_MAX_DEPTH)
+    elif evaluate(board_class.board) * player < 14: 
         # Opponent has 2 units left
-        optimal_move = minimax(board_ndarray, all_moves, player, MID_MAX_DEPTH)
+        optimal_move = minimax(board_class, all_moves, player, MID_MAX_DEPTH)
     else:   
         # Opponent has one unit left
-        optimal_move = minimax(board_ndarray, all_moves, player, END_MAX_DEPTH)
+        optimal_move = minimax(board_class, all_moves, player, END_MAX_DEPTH)
     x, y, u, v = optimal_move
     update_board_my_turn(board_ndarray, (x, y, u, v))
     return (x, y), (u, v)
+
