ac3f2368135fcf45d6652d1cdc44e0d009e906c3
thanhtoan1742 <thanhtoan1742@gmail.com>
2021-05-25 14:27:08 +0700

    added timer to game

--- src/game.py
+++ src/game.py
@@ -1,3 +1,4 @@
+import time
 from board import *
 from random_player import move as random_move_maker_1
 from random_player import move_2 as random_move_maker_2
@@ -8,7 +9,7 @@ class game:
         self.first_move_maker = first_move_maker
         self.second_move_maker = second_move_maker
 
-    def run(self, verbose=0):
+    def run(self, verbose=0, limit_time=False):
         current_move_maker = self.first_move_maker
         next_move_maker = self.second_move_maker
         while 1:
@@ -17,8 +18,18 @@ class game:
             if self.board.is_finished():
                 break
 
-            c_player = 'X' if self.board.get_current_player() == 1 else 'O'
+
+
+            begin_time = time.perf_counter()
             move = current_move_maker(self.board.get_board(), self.board.get_current_player())
+            duration = time.perf_counter() - begin_time
+            if verbose > 0:
+                print('time: %.4f' % duration)
+            if duration > 3 and limit_time:
+                raise Exception('time limit exceeded, your time is %4f' % duration)
+
+
+            c_player = 'X' if self.board.get_current_player() == 1 else 'O'
             print(f"Player {c_player} moves from ({move[0][0]}, {move[0][1]}) to ({move[1][0]}, {move[1][1]})")
             self.board.make_move(move)
 
--- src/main.py
+++ src/main.py
@@ -3,6 +3,6 @@ from input_player import move as input_move_maker
 from random_player import move as random_move_maker
 from minimax_player import move as minimax_move_maker
 
-# g = game(first_move_maker=input_move_maker)
-g = game()
+g = game(first_move_maker=input_move_maker)
+# g = game()
 g.run(verbose=1)
